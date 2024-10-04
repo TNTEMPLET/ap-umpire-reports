10422a92b09c2b85af81a4a8e03bd3e0f7d53d76
tntemplet <tntemplet@gmail.com>
2024-10-04 08:36:54 -0500

    Updated config

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 export const client_id = "KYX4Fa85OCO1DTjMS0ji-OoGaf3TISfnvnzgY0kpYxg";
 export const client_secret = "DfRIvDtg9gq0Kw0k2pRQVh6frPm6T0A573wPtU8zvnY";
 export const assignr_url = "https://api.assignr.com/api";
-export const cors_url = "https://cors-anywhere.herokuapp.com/";
-export const redirect_uri = "https://tntemplet.github.io/ap-umpire-reports/"
