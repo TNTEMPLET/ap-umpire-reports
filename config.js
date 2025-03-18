15ee556408af2eddeadaebcf2da039809d76cec7
tntemplet <tntemplet@gmail.com>
2025-03-18 09:57:09 -0500

    added apiFunctions updated other files

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 export const client_id = "5zRshwho1cwc2dKW6B58EHpczSEeaCTfv7GAATRMlDw";
 export const client_secret = "QADrkiBQcx9SA2Udf4KCzRRu24c4vdZUrcADLs-eku8";
 export const assignr_url = "https://api.assignr.com/api";
+export const devBaseUrl = "http://localhost:3100";
+export const prodBaseUrl = "https://apreports.netlify.app";
